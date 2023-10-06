1635b96d704aee2cc14e04691bb4065d4a50b361
Luis David <luisdavidortegasoto@gmail.com>
2023-10-06 17:04:09 -0400

    cambios

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base:"https://luisdavid06.github.io/website-test/",
   resolve: {
     alias: [{ find: "@", replacement: "/src" }],
   },
